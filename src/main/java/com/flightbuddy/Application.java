272e8778dffc22e0220edee411419fe70190bf62
maciekwilk <maciekkwilk@gmail.com>
2016-12-25 16:46:21 +0100

    removing @EnableScheduling

--- src/main/java/com/flightbuddy/Application.java
+++ src/main/java/com/flightbuddy/Application.java
@@ -3,10 +3,8 @@ package com.flightbuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @PropertySource({"classpath:environment.properties"})
 public class Application {
 
