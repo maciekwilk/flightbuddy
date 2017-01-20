102c6abed4123ca0816957787a6d4791f0105fff
maciekwilk <maciekkwilk@gmail.com>
2017-01-20 22:55:39 +0100

    changes in integration tests

--- src/test/java/com/flightbuddy/google/GoogleServiceIT.java
+++ src/test/java/com/flightbuddy/google/GoogleServiceIT.java
@@ -10,64 +10,68 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.flightbuddy.SearchInputData;
-import com.flightbuddy.exceptions.NoFlightsFoundException;
 import com.flightbuddy.google.response.GoogleResponse;
 import com.flightbuddy.google.response.Trips;
 import com.flightbuddy.google.response.tripoption.TripOption;
+import com.flightbuddy.google.schedule.GoogleTask;
 import com.flightbuddy.results.FoundTrip;
 
-@RunWith(MockitoJUnitRunner.class)
-public class GoogleServiceTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleServiceIT {
 	
+	@Autowired
 	private GoogleService googleService;
 	
-	@Mock
+	@MockBean
 	private GoogleConnectionService googleConnectionService;
-	@Mock
+	@MockBean
 	private GoogleFlightConverter googleFlightConverter;
+	@MockBean
+	private GoogleTask googleTask;
 	
-	@Before
-	public void setUp() {
-		googleService = new GoogleService();
-		googleService.setGoogleConnectionService(googleConnectionService);
-		googleService.setGoogleFlightConverter(googleFlightConverter);
-	}
-	
-	@Test(expected = NoFlightsFoundException.class)
+	@Test
 	public void checkGetGoogleFlightsWithEmptyGoogleConnectionServiceResponse() {
-		when(googleConnectionService.askGoogleForTheTrips(any())).thenReturn(new GoogleResponse());
-		 googleService.getGoogleTrips(new SearchInputData());
+		GoogleResponse emptyGoogleResponse = new GoogleResponse();
+		when(googleConnectionService.askGoogleForTheTrips(any())).thenReturn(emptyGoogleResponse);
+		googleService.getTrips(new SearchInputData());
+		verify(googleFlightConverter, times(1)).convertResponseToTrips(emptyGoogleResponse);
 	}
 	
-	@Test(expected = NoFlightsFoundException.class)
+	@Test
 	public void checkGetGoogleFlightsWithGoogleConnectionServiceResponseWithNullTripOption() {
 		Trips trips = createTrips(null);
-		mockGoogleConnectionService(trips);
-		googleService.getGoogleTrips(new SearchInputData());
+		GoogleResponse googleResponse = createGoogleResponse(trips);
+		when(googleConnectionService.askGoogleForTheTrips(any())).thenReturn(googleResponse);
+		googleService.getTrips(new SearchInputData());
+		verify(googleFlightConverter, times(1)).convertResponseToTrips(googleResponse);
 	}
 	
-	@Test(expected = NoFlightsFoundException.class)
+	@Test
 	public void checkGetGoogleFlightsWithGoogleConnectionServiceResponseWithEmptyTripOption() {
 		Trips trips = createTrips(new TripOption[0]);
-		mockGoogleConnectionService(trips);
-		googleService.getGoogleTrips(new SearchInputData());
+		GoogleResponse googleResponse = createGoogleResponse(trips);
+		when(googleConnectionService.askGoogleForTheTrips(any())).thenReturn(googleResponse);
+		googleService.getTrips(new SearchInputData());
+		verify(googleFlightConverter, times(1)).convertResponseToTrips(googleResponse);
 	}
 	
 	@Test
 	public void checkGetGoogleFlightsWithValidGoogleConnectionServiceResponse() {
-		TripOption[] tripOption = new TripOption[1];
-		tripOption[0] = new TripOption();
+		TripOption[] tripOption = new TripOption[]{new TripOption()};
 		Trips trips = createTrips(tripOption);
-		GoogleResponse googleResponse = mockGoogleConnectionService(trips);
+		GoogleResponse googleResponse = createGoogleResponse(trips);
+		when(googleConnectionService.askGoogleForTheTrips(any())).thenReturn(googleResponse);
 		List<FoundTrip> conversionResult = mockGoogleFlightConverter(googleResponse);
-		List<FoundTrip> result = googleService.getGoogleTrips(new SearchInputData());
+		List<FoundTrip> result = googleService.getTrips(new SearchInputData());
 		verify(googleFlightConverter, times(1)).convertResponseToTrips(googleResponse);
 		assertThat(result, equalTo(conversionResult));
 	}
@@ -81,12 +85,6 @@ public class GoogleServiceTest {
 	private GoogleResponse createGoogleResponse(Trips trips) {
 		GoogleResponse googleResponse = new GoogleResponse();
 		googleResponse.setTrips(trips);
-		return googleResponse;
-	}
-
-	private GoogleResponse mockGoogleConnectionService(Trips trips) {
-		GoogleResponse googleResponse = createGoogleResponse(trips);
-		when(googleConnectionService.askGoogleForTheTrips(any())).thenReturn(googleResponse);
 		return googleResponse;
 	}
 
