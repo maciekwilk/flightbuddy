4b366e045832d99b6b6707acc3186517c085d7d9
maciekwilk <maciekkwilk@gmail.com>
2017-04-17 21:14:14 +0200

    adding angularjs

--- src/main/java/com/flightbuddy/MainController.java
+++ src/main/java/com/flightbuddy/MainController.java
@@ -1,14 +1,20 @@
 package com.flightbuddy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/flightbuddy")
 public class MainController {
 	
-	@RequestMapping("/")
-	public void handleaTrips() {
-		
+	@RequestMapping("/resource")
+	public Map<String,Object> home() {
+	    Map<String,Object> model = new HashMap<String,Object>();
+	    model.put("id", UUID.randomUUID().toString());
+	    model.put("content", "Hello World");
+	    return model;
 	}
 }
