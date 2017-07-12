0da3efdd2cf4cf3bbd0bef5eeff091204d399db7
maciekwilk <maciekkwilk@gmail.com>
2017-07-12 23:19:27 +0200

    preparations for first release

--- src/main/java/com/flightbuddy/Application.java
+++ src/main/java/com/flightbuddy/Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource({"classpath:environment.properties"})
+@PropertySource({"classpath:environment.properties", 
+	"file:${user.home}/flightbuddyData/environment.properties"})
 public class Application {
 
     public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class Application {
     	properties.setProperty("logging.level.com.flightbuddy.interceptors.LoggingRestInterceptor", "DEBUG");
     	properties.setProperty("spring.aop.proxy-target-class", "true");
     	properties.setProperty("spring.jackson.serialization.write_dates_as_timestamps", "false");
+    	properties.setProperty("logging.file", "${user.home}/flightbuddy/logs/console.log");
     	new SpringApplicationBuilder(Application.class).properties(properties).run(args);
     }
 }
